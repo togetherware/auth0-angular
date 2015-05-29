0903041c971c41c52df7b2db6f2c9cdfe2f3400c
Peter Stromquist <peter@twistedstream.com>
2015-05-29 16:13:10 -0500

    Incorporate UI Router

--- examples/widget-ui-router/scripts/controllers.js
+++ examples/widget-ui-router/scripts/controllers.js
@@ -1,55 +1,67 @@
 var myApp = angular.module('myApp');
 
-myApp.controller('MenuCtrl', function ($scope, $location, auth, store) {
-  $scope.go = function (target) {
-    $location.path(target);
-  };
+myApp.controller('MsgCtrl', function ($scope) {
+  console.log('MsgCtrl');
 
-  var saveUserInfo = function(profile, token) {
-    store.set('profile', profile);
-    store.set('token', token);
-  }
-
-  $scope.signup = function() {
-    auth.signup({popup:  true, auto_login: false})
-      .then(function(profile, id_token) {
-        saveUserInfo(profile, id_token);
-        $location.path('/');
-
-      })
-  }
-
-  $scope.reset = function () {
-    auth.reset({}, function () {
-        // TODO Handle when login succeeds
-        console.log("OK");
-      }, function () {
-        console.log("FAIL");
-        // TODO Handle when login fails
-      });
-  };
-
-  $scope.login = function () {
-    auth.signin({}, function (profile, id_token) {
-        saveUserInfo(profile, id_token);
-        $location.path('/');
-      }, function () {
-        // TODO Handle when login fails
-      });
-  };
+  $scope.message = '';
 });
 
 myApp.controller('RootCtrl', function (auth, $scope) {
+  console.log('RootCtrl');
+  $scope.$parent.message = '';
   $scope.auth = auth;
 });
 
-myApp.controller('LoginCtrl', function (auth, $scope) {
-  $scope.auth = auth;
+var saveUserInfo = function(profile, token, store) {
+  store.set('profile', profile);
+  store.set('token', token);
+};
+
+myApp.controller('LoginCtrl', function (auth, $scope, $state, store) {
+  console.log('LoginCtrl');
+
+  $scope.$parent.message = 'loading signin...';
+
+  auth.signin({}, function (profile, id_token) {
+    saveUserInfo(profile, id_token, store);
+    $state.go('root');
+  }, function () {
+    $scope.$parent.message = 'invalid credentials';
+    $scope.loading = false;
+  });
 });
 
-myApp.controller('LogoutCtrl', function (auth, $location, store) {
+myApp.controller('SignupCtrl', function (auth, $scope, $state, store) {
+  console.log('SignupCtrl');
+
+  $scope.$parent.message = 'loading signup...';
+
+  auth.signup({ popup:  true, auto_login: false }, function (profile, id_token) {
+    saveUserInfo(profile, id_token, store);
+    $state.go('root');
+  }, function  () {
+    console.log('foo');
+  });
+});
+
+myApp.controller('ResetCtrl', function (auth, $scope) {
+  console.log('ResetCtrl');
+
+  $scope.$parent.message = 'loading password reset...';
+
+  auth.reset({}, function () {
+    console.log('reset success');
+  }, function  () {
+    console.log('reset fail');
+  });
+});
+
+myApp.controller('LogoutCtrl', function (auth, $location, store, $scope, $state) {
+  console.log('LogoutCtrl');
+
   auth.signout();
   store.remove('profile');
   store.remove('token');
-  $location.path('/login');
+  $scope.$parent.message = '';
+  $state.go('root');
 });
